274196d250e6f77e5d696e757dc8449d598ad61c
Your Name <nomi4698dg@gmail.com>
2024-05-14 04:46:01 -0700

    todo

--- app/page.js
+++ app/page.js
@@ -1,113 +1,171 @@
-import Image from "next/image";
+"use client"
+import { MdDelete } from "react-icons/md";
+import { MdEdit } from "react-icons/md";
+import { MdAddToPhotos } from "react-icons/md";
+import { MdCancel } from "react-icons/md";
 
-export default function Home() {
+import { useState } from "react";
+
+export default function Page() {
+  const [opencart, setOpencart] = useState(false)
+  const [array, setArray] = useState([])
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [adress, setAdress] = useState("")
+  const [phone, setPhone] = useState("")
+  const [orr, setOrr] = useState("")
+  const [userindex, setUserindex] = useState(null)
+
+
+  const open = () => {
+    setOpencart(true)
+  }
+  const adduser = () => {
+    var username = name.trim()
+    var useremail = email.trim()
+    var useradress = adress.trim()
+    var userphone = phone.trim()
+    if (username.length < 3 || useremail.length < 3 || useradress.length < 3 || userphone.length < 3) {
+      setOrr("Please fill in all fields.")
+    } else {
+      var userobj = {
+        no: name,
+        em: email,
+        adr: adress,
+        pho: phone,
+      }
+      setArray([...array, userobj])
+      setName("");
+      setEmail("");
+      setAdress("");
+      setPhone("");
+      setOrr("")
+      setOpencart(false)
+    }
+  }
+
+  var deleteUser = (i) => {
+    const updatedItems = array.filter((item, ind) => ind != i)
+    setArray(updatedItems)
+  }
+
+  const edititem = (item, i) => {
+   
+    setName(item.no);
+    setEmail(item.em);
+    setAdress(item.adr);
+    setPhone(item.pho);
+    setUserindex(i);
+    setOpencart(true);
+  };
+
+  const update = () => {
+    if (name.length < 3 || email.length < 3 || adress.length < 3 || phone.length < 3) {
+      setOrr("Please fill in all fields.");
+      return;
+    }
+
+    const newArray = [...array];
+
+    newArray[userindex] = {
+      no: name,
+      em: email,
+      adr: adress,
+      pho: phone,
+    };
+
+    setArray(newArray);
+    setOpencart(false);
+    setUserindex(null);
+    setName("");
+    setEmail("");
+    setAdress("");
+    setPhone("");
+    setOrr("");
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className="flex justify-center mt-[45px] ">
+        <div className="h-[500px] w-[810px] bg-[#d8d6d6] overflow-auto drop-shadow-2xl">
+          <div className="h-[50px] w-[810px] bg-[#435D7D] fixed">
+            <div className="flex items-center justify-around   ">
+              <h1 className="text-[white] font-bold  mt-[12px] " >Manage Employees</h1>
+              <button className="h-[30px] w-[200px] text-[white] bg-[#28a745] rounded-md ml-[300px] mt-[10px] " onClick={open} > <div className="flex justify-evenly items-center"><MdAddToPhotos className="text-[21px] " /> Add Employees </div></button>
+
+            </div>
+          </div>
+
+          <table className=" w-[100%] border border-solid border-black border-collapse mt-[50px]	 ">
+            <thead>
+            <tr >
+              
+              <th className='border border-gray-300 text-left p-2 bg-gray-200'>No</th>
+              <th className='border border-gray-300 text-left p-2 bg-gray-200'>	Name</th>
+              <th className='border border-gray-300 text-left p-2 bg-gray-200' >Email</th>
+              <th className='border border-gray-300 text-left p-2 bg-gray-200'>Address</th>
+              <th className='border border-gray-300 text-left p-2 bg-gray-200'>Phone</th>
+              <th className='border border-gray-300 text-left p-2 bg-gray-200'>Actions</th>
+            </tr>
+            </thead>
+            {
+              array.map((user, i) => {
+                return (
+                  <tbody  key={i}>
+                  <tr >
+                    <td className='border border-gray-300 text-left p-2 bg-gray-200' >{i + 1}</td>
+                    <td className='border border-gray-300 text-left p-2 bg-gray-200'>{user.no.length > 13 ? user.no.substring(0, 13) + '...' : user.no}</td>
+                    <td className='border border-gray-300 text-left p-2 bg-gray-200' >{user.em.length > 13 ? user.em.substring(0,13) + '...' : user.em}</td>
+                    <td className='border border-gray-300 text-left p-2 bg-gray-200'>{user.adr.length > 13 ? user.adr.substring(0,13) + '...' : user.adr}</td>
+                    <td className='border border-gray-300 text-left p-2 bg-gray-200'>{user.pho.length > 13 ? user.pho.substring(0,13) + '...' : user.pho}</td>
+                    <td className='border border-gray-300 text-left p-2 bg-gray-200 '>
+                      <div className=" flex justify-around">
+                        <MdEdit className="text-[green] text-[21px] cursor-pointer" onClick={() => edititem(user, i)} /> <MdDelete className="text-[red] text-[21px] cursor-pointer" onClick={() => deleteUser(i)} />
+                      </div>
+                    </td>
+
+                  </tr>
+                  </tbody>
+                )
+              })
+            }
+          </table>
         </div>
       </div>
+      {opencart &&
+        <div className="h-full drop-shadow-2xl w-full bg-[#7a7a7a69] absolute top-[0px] left-[0px]">
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+          <div className="h-[430px] drop-shadow-2xl w-[400px] bg-[#345c8b] absolute top-[65px] left-[450px] ">
+            <div className="flex justify-around">
+              <h1 className="font-bold text-[white] mt-[15px] ">Add Employee</h1>
+              <button className="text-[white] ml-[190px] mt-[15px]" onClick={() => close(setOpencart(false))}><MdCancel className="text-[21px]" /></button>
+            </div>
+            <hr className="mt-[10px]" />
+            <label>
+              <h1 className="text-[white] ml-[45px] mt-[15px]">Name</h1>
+              <input type="text" className="h-[30px] w-[300px] ml-[45px] mt-[5px] outline-none pl-[10px] rounded-[5px]" onChange={(e) => { setName(e.target.value) }} value={name} />
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+              <h1 className="text-[white] ml-[45px] mt-[15px]">Email</h1>
+              <input type="email" className="h-[30px] w-[300px] ml-[45px] mt-[5px] outline-none pl-[10px] rounded-[5px]" onChange={(e) => { setEmail(e.target.value) }} value={email} />
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+              <h1 className="text-[white] ml-[45px] mt-[15px]">Adress</h1>
+              <input type="text" className=" h-[30px] w-[300px] ml-[45px] mt-[5px] outline-none pl-[10px] rounded-[5px]" onChange={(e) => { setAdress(e.target.value) }} value={adress} />
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+              <h1 className="text-[white] ml-[45px] mt-[15px]">Phone</h1>
+              <input type="number" className=" h-[30px] w-[300px] ml-[45px] mt-[5px] outline-none pl-[10px] rounded-[5px] " onChange={(e) => { setPhone(e.target.value) }} value={phone} />
+            </label>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+            <p className="text-[red] ml-[80px]">{orr ? orr : ""}</p>
+            {userindex !== null ?
+              <button className="bg-[#28a745] h-[30px] w-[100px] text-[white] ml-[150px] mt-[20px] rounded-[5px]" onClick={update}>update</button> :
+              <button className="bg-[#28a745] h-[30px] w-[100px] text-[white] ml-[150px] mt-[20px] rounded-[5px]" onClick={adduser}>submite</button>
+            }
+          </div>
+
+
+        </div>
+      }
+    </>
+
   );
 }
